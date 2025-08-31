07a72b0f4261a1a964d7864096901f88903ab9ae
JOJOMDS11 <padrejojos@gmail.com>
2025-08-31 20:21:03 -0300

    Update gameConfigs.js

--- gameConfigs.js
+++ gameConfigs.js
@@ -27,16 +27,16 @@ const gameConfigs = {
   },
   '4x4': {
     futsal: {
-      backgroundImage: 'https://i.imgur.com/UiOK7Gr.png',
+      backgroundImage: 'https://i.imgur.com/dZgIa0e.png',
       players: [
         { uid: 1, id: 'GK', team: 'red', x: 0.1, y: 0.5 },
-        { uid: 2, id: 'DEF', team: 'red', x: 0.3, y: 0.3 },
-        { uid: 3, id: 'MID', team: 'red', x: 0.3, y: 0.7 },
-        { uid: 4, id: 'ATT', team: 'red', x: 0.45, y: 0.5 },
+        { uid: 2, id: 'VL', team: 'red', x: 0.3, y: 0.3 },
+        { uid: 3, id: 'MC', team: 'red', x: 0.3, y: 0.7 },
+        { uid: 4, id: 'PV', team: 'red', x: 0.45, y: 0.5 },
         { uid: 5, id: 'GK', team: 'blue', x: 0.9, y: 0.5 },
-        { uid: 6, id: 'DEF', team: 'blue', x: 0.7, y: 0.3 },
-        { uid: 7, id: 'MID', team: 'blue', x: 0.7, y: 0.7 },
-        { uid: 8, id: 'ATT', team: 'blue', x: 0.55, y: 0.5 },
+        { uid: 6, id: 'VL', team: 'blue', x: 0.7, y: 0.3 },
+        { uid: 7, id: 'MC', team: 'blue', x: 0.7, y: 0.7 },
+        { uid: 8, id: 'PV', team: 'blue', x: 0.55, y: 0.5 },
         { uid: 10, id: '', team: 'ball', x: 0.5, y: 0.5 }
       ]
     }
