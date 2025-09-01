5cf79924c31f5888c9cc8ea3b2e208e496fac1d3
JOJOMDS11 <padrejojos@gmail.com>
2025-08-31 21:00:49 -0300

    Update gameConfigs.js

--- gameConfigs.js
+++ gameConfigs.js
@@ -41,6 +41,22 @@ const gameConfigs = {
       ]
     }
   },
+   '4x4': {
+    bigeasy: {
+      backgroundImage: 'https://i.imgur.com/dZgIa0e.png',
+      players: [
+        { uid: 1, id: 'GK', team: 'red', x: 0.1, y: 0.5 },
+        { uid: 2, id: 'VL', team: 'red', x: 0.3, y: 0.3 },
+        { uid: 3, id: 'MC', team: 'red', x: 0.3, y: 0.7 },
+        { uid: 4, id: 'PV', team: 'red', x: 0.45, y: 0.5 },
+        { uid: 5, id: 'GK', team: 'blue', x: 0.9, y: 0.5 },
+        { uid: 6, id: 'VL', team: 'blue', x: 0.7, y: 0.3 },
+        { uid: 7, id: 'MC', team: 'blue', x: 0.7, y: 0.7 },
+        { uid: 8, id: 'PV', team: 'blue', x: 0.55, y: 0.5 },
+        { uid: 10, id: '', team: 'ball', x: 0.5, y: 0.5 }
+      ]
+    }
+  },
   '7x7': {
     futsal: {
       backgroundImage: 'https://i.imgur.com/n56z593.png',
